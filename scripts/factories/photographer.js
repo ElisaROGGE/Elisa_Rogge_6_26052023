d071dbede46b4841e678ac78d53952b917e2f951
Elisa ROGGE <elisa.rogge59@gmail.com>
2023-07-28 09:54:57 +0200

    accessibility

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,29 +1,32 @@
 function photographerFactory(data) {
     const { name, portrait, city, country, tagline, price } = data;
-
+  
     const picture = `assets/photographers/Photographers/${portrait}`;
-
+  
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        const location = document.createElement( 'span' );
-        location.textContent = city + ', ' + country;
-        location.className = "location"
-        const taglineText = document.createElement( 'span' );
-        taglineText.textContent = tagline;
-        taglineText.className = 'tagline'
-        const priceText = document.createElement( 'span' );
-        priceText.textContent = price + '€/jour';
-        priceText.className = 'price'
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(location);
-        article.appendChild(taglineText);
-        article.appendChild(priceText);
-        return (article);
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      img.setAttribute('src', picture);
+      img.setAttribute('alt', `${name} - Portrait`);
+      const h2 = document.createElement('h2');
+      h2.textContent = name;
+      const location = document.createElement('p');
+      location.textContent = `${city}, ${country}`;
+      location.className = 'location';
+      const taglineText = document.createElement('p');
+      taglineText.textContent = tagline;
+      taglineText.className = 'tagline';
+      const priceText = document.createElement('p');
+      priceText.textContent = `${price}€/jour`;
+      priceText.className = 'price';
+      article.appendChild(img);
+      article.appendChild(h2);
+      article.appendChild(location);
+      article.appendChild(taglineText);
+      article.appendChild(priceText);
+      return article;
     }
-    return { name, picture, getUserCardDOM }
-}
+  
+    return { name, picture, getUserCardDOM };
+  }
+  
